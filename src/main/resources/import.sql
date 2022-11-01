ead8e33f7ca3acc8020246af84d3008947328e90
Carlos Padoa <samuraicharlie@gearsofcode.tech>
2022-11-01 15:59:36 -0300

    Fixed SQL column names

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
-INSERT INTO Owner (id, firstName, lastName, address, city, telephone) VALUES (1, 'Charlie', 'Silver', '5th avenue', 'NY', '3555-1288');
-INSERT INTO Owner (id, firstName, lastName, address, city, telephone) VALUES (1, 'Steven', 'Brooks', '7th avenue', 'NY', '3666-1288');
+INSERT INTO Owner (id, first_Name, last_Name, address, city, telephone) VALUES (1, 'Charlie', 'Silver', '5th avenue', 'NY', '3555-1288');
+INSERT INTO Owner (id, first_Name, last_Name, address, city, telephone) VALUES (2, 'Steven', 'Brooks', '7th avenue', 'NY', '3666-1288');
